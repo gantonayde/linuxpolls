996ec5568c45de4116b06fc706b9801e3941cc28
Genadi Naydenov <gantonayde@gmail.com>
2020-06-24 23:34:58 +0100

    Added search page

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList, name='home'), 
     path('about/', views.About , name='about'),
+    path('search/', views.Search , name='search'),
     path('articles/', views.Articles, name='articles_list'),   
     path('articles/<slug:slug>/', views.post_detail, name='post_detail'),  
     
--- articles/views.py
+++ articles/views.py
@@ -13,24 +13,8 @@ from django.urls import reverse
 
 
 def PostList(request):
-
-    search_terms = request.GET.get("q")
-    print(search_terms)
-   # query = ""
-   ## if request.GET:
-        
-       # query = request.GET['q']
-       # query = str(query)
-
-    
-    #plot_div = add_figure(Question.objects.get(pk=2),1)
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    
-   # object_list = get_article_queryset(query)
-    if search_terms:
-        object_list = Post.objects.filter(Q(title__icontains=search_terms) | Q(content__icontains=search_terms)).order_by('-created_on')
-    else:
-        object_list = Post.objects.filter(status=1).order_by('-created_on')
+    object_list = Post.objects.filter(status=1).order_by('-created_on')
     paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -52,18 +36,9 @@ def PostList(request):
                    'main_question': main_question,
         })
 
-    # return render(request,
-    #               'index.html',
-    #               {'page': page,
-    #                'post_list': post_list,
-    #                'latest_question_list': latest_question_list,
-    #                'main_question': main_question},)
 
 def Articles(request):
- 
-    #plot_div = add_figure(Question.objects.get(pk=2),1)
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    
     object_list = Post.objects.filter(status=1).order_by('-created_on')
     paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
@@ -80,6 +55,38 @@ def Articles(request):
                   {'page': page,
                    'post_list': post_list,
                    'latest_question_list': latest_question_list},)
+
+def Search(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    search_terms = request.GET.get("s")
+    if search_terms:
+        object_list = Post.objects.filter(Q(title__icontains=search_terms) | 
+                                          Q(content__icontains=search_terms)).order_by('-created_on')
+        if not object_list:
+            print("No matches")
+    else:
+        print("else")
+        object_list = Post.objects.none()
+    print(search_terms)
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+        print(post_list)
+    except PageNotAnInteger:
+            # If page is not an integer deliver the first page
+        post_list = paginator.page(1)
+        for i in post_list:
+            print(i,post_list)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        post_list = paginator.page(paginator.num_pages)
+    return render(request,
+                  'search.html',
+                  {'page': page,
+                   'post_list': post_list,
+                   'latest_question_list': latest_question_list,
+                   'search_terms': search_terms},)
 
 def About(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]

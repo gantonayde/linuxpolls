6cedbcad58913e516b84e1238b650f97bdb6eedb
Genadi Naydenov <gantonayde@gmail.com>
2020-08-14 16:51:47 +0100

    Switch from jpeg to webp

--- articles/migrations/0012_post_category.py
+++ articles/migrations/0012_post_category.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='post',
             name='category',
-            field=models.ForeignKey(blank=True, default=1, on_delete=django.db.models.deletion.CASCADE, to='articles.Category'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='articles.Category'),
             preserve_default=False,
         ),
     ]
